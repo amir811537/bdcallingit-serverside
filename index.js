77a59d3bd89c59e03cf8a10eab2d92cfc4eecd36
amir811537 <amirhossain.bc.75@gmail.com>
2024-05-22 23:47:02 +0600

    addd product type api

--- index.js
+++ index.js
@@ -89,17 +89,28 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    // Get products by brand
-    app.get("/products/:brand", async (req, res) => {
-      const brandName = req.params.brand;
-
-      const query = { brandname: brandName.toLowerCase() };
-      // console.log(query)
-      const cursor = productCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+    
+    app.get("/products/type/:type", async (req, res) => {
+      const type = req.params.type;
+      console.log(`Received request for type: ${type}`);
+    
+      try {
+        const query = { type: new RegExp(`^${type}$`, 'i') };
+        const cursor = productCollection.find(query);
+        const result = await cursor.toArray();
+    
+        if (result.length === 0) {
+          console.log("No products found for type:", type);
+          return res.status(404).send({ message: "No products found." });
+        }
+    
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
     });
+    
 
     // get singel product by id
     app.get("/productsbyid/:id", async (req, res) => {
@@ -141,13 +152,13 @@ async function run() {
       res.send(result);
     });
     //user related api post
-    app.post("/user", async (req, res) => {
+    app.post("/userCart", async (req, res) => {
       const user = req.body;
       const result = await usercollection.insertOne(user);
       res.send(result);
     });
 //  user get with email and products
-    app.get("/userProduct/:email", async (req, res) => {
+    app.get("/userCart/:email", async (req, res) => {
       const email=req.params.email;
       const query = { email: email };
       const cursor = usercollection.find(query);
@@ -157,7 +168,7 @@ async function run() {
 
 
 //  user delete 
-app.delete('/user/:id',async(req,res)=>{
+app.delete('/userCart/:id',async(req,res)=>{
   const id=req.params.id;
   const query={_id: new ObjectId(id)}
   const result= await usercollection.deleteOne(query);
